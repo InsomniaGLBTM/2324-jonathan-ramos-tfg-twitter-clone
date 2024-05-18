171ff93c4350619a0c4169b9c48580b23fdff2ab
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2024-05-18 17:56:53 +0200

    Edit signup and login server actions Add user.utils

--- core/actions/auth/login.ts
+++ core/actions/auth/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { LOGIN_REDIRECT } from '@/core/constants/authRoutes.constants';
+import { signIn } from '@/core/services/nextAuth/auth.service';
 import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 
 export const login = async (data: z.infer<typeof LoginSchema>) => {
@@ -9,5 +12,23 @@ export const login = async (data: z.infer<typeof LoginSchema>) => {
   if (!validateFields.success) {
     return { error: 'error' };
   }
-  return { success: 'success' };
+  const { email, password } = validateFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'credentialsError}' };
+        default:
+          return { error: 'error}' };
+      }
+    }
+    throw error;
+  }
 };
--- core/actions/auth/signup.ts
+++ core/actions/auth/signup.ts
@@ -1,13 +1,36 @@
 'use server';
 
+import { getUserByEmail } from '@/core/utils/db/user.utils';
+import { db } from '@/lib/db';
 import { SignupSchema } from '@/schemas';
+import bcryptjs from 'bcryptjs';
 import { z } from 'zod';
 
 export const sigup = async (data: z.infer<typeof SignupSchema>) => {
   const validateFields = SignupSchema.safeParse(data);
-  console.log(data);
+
   if (!validateFields.success) {
     return { error: 'error' };
   }
+
+  const { email, name, password, username } = validateFields.data;
+
+  const hashedPassword = await bcryptjs.hash(password, 10);
+
+  const user = await getUserByEmail(email);
+
+  if (user) {
+    return { error: 'user' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
   return { success: 'success' };
 };
