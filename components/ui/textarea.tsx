733e2ab1e049d06e5ca457d8d151f84641979d3d
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2024-05-21 21:59:40 +0200

    Add shadcn ui textarea

---
+++ components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          'resize-none outline-none  flex w-full bg-background ring-offset-background placeholder:text-gray-400 text-xl  disabled:cursor-not-allowed disabled:opacity-50',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = 'Textarea';
+
+export { Textarea };
