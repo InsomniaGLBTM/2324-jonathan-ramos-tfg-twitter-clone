311331d866b755043a6cd3823fc8829fde7d2acc
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2024-05-21 21:53:38 +0200

    Add iconSize constants

--- core/components/auth/AuthContainer.tsx
+++ core/components/auth/AuthContainer.tsx
@@ -1,8 +1,9 @@
+import { IconSize } from '@/core/constants/iconSize.constants';
 import { FaTwitter } from 'react-icons/fa';
 export const AuthContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-black rounded-xl min-h-40 flex flex-col place-items-center pt-4 pb-12 px-20">
-      <FaTwitter size={50} className="text-sky-500 mb-6" />
+      <FaTwitter size={IconSize.auth} className="text-sky-500 mb-6" />
       {children}
     </div>
   );
---
+++ core/constants/iconSize.constants.ts
@@ -0,0 +1,7 @@
+export const IconSize = {
+  auth: 50,
+  mainApp: {
+    navBar: 31,
+    post: 15,
+  },
+};
