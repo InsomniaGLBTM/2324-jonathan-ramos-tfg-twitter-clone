64557b8d68590a7659afd7ae2a097581465b06c0
JonathanAEG <jonathan.ramos@ikasle.aeg.eus>
2024-05-27 17:42:15 +0200

    Add locale from cookies

--- app/[locale]/(auth)/signup/page.tsx
+++ app/[locale]/(auth)/signup/page.tsx
@@ -2,14 +2,12 @@ import { SignupForm } from '@/core/components/auth/SignupForm';
 import { Namespaces } from '@/core/constants/namespaces.constants';
 import TranslationsProvider from '@/core/providers/i18n/TranslationsProvider';
 import initTranslations from '@/core/services/i18n/i18n.service';
+import { getLocaleFromCookies } from '@/core/utils/cookies.utils';
 
 const i18nNamespaces = [Namespaces.AUTH, Namespaces.COMMON];
 
-export default async function Signup({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
+export default async function Signup() {
+  const locale = getLocaleFromCookies();
   const { resources } = await initTranslations(locale, i18nNamespaces);
   return (
     <TranslationsProvider
